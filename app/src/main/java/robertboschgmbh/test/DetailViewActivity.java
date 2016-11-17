2a9a83064f64f26f652013f13337829a24d662d5
Lukas Schult <glukasschult@gmail.com>
2016-11-17 10:50:48 +0100

    added menu on detailviewactivity

--- app/src/main/java/robertboschgmbh/test/DetailViewActivity.java
+++ app/src/main/java/robertboschgmbh/test/DetailViewActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -73,6 +75,32 @@ public class DetailViewActivity extends AppCompatActivity {
         updateBlocks();
         checkButtonVisibility();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Intent i = new Intent(this,LoginActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        else return false;
     }
 
     private void fillViewSets() {
